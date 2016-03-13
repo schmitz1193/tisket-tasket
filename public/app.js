1b7f49c76baecc18924ba6d6d4177c064aa389a1
schmitz1193 <schmitz1193@gmail.com>
2016-03-13 15:30:10 -0500

    login partial

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
-r app = angular.module("MaiApp", ["ngRoute", "firebase", "angular.filter", "ui.calendar"]);
+var app = angular.module('MainApp', ['ui.router'])
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: '/partials/login.html',
+      controller: '/controllers/loginCtrl'
+    });
+    // .state('posts', {
+    //   url: '/posts/{id}',
+    //   templateUrl: '/posts.html',
+    //   controller: 'PostsCtrl'
+    // });
+
+  $urlRouterProvider.otherwise('login');
+}]);
