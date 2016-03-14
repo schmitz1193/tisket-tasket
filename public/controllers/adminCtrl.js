029980d2ba0f5ab3c9321459c673a424d7d8a29c
schmitz1193 <schmitz1193@gmail.com>
2016-03-14 18:06:19 -0500

    admin data

--- public/controllers/adminCtrl.js
+++ public/controllers/adminCtrl.js
@@ -5,6 +5,14 @@ app.controller("AdminCtrl", [
   '$scope',
 
   function($scope){
-      console.log("I'm in charge of shops!");
+    console.log("I'm in charge of shops!");
+    const title = '';
+    const link = '';
+    $scope.addWebsite = function() {
+      console.log("title ", $scope.title);
+      console.log("link ", $scope.link);
+
+      //now this data needs to be sent to the server  to add to the db
+    }
   }
 ]);
