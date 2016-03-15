6276ec8b1d51a94019787db5c08973963b5544bb
schmitz1193 <schmitz1193@gmail.com>
2016-03-15 12:01:33 -0500

    add GET to server

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;  //set up environment port if none given default to 3000
 
 //test the server set route to index page
-// app.get('/', function (req,res) {
-//   res.send('Tisket Tasket keep me from the casket!')
-// });
+app.get('/', function (req,res) {
+  res.send('Tisket Tasket keep me from the casket!')
+});
 
 
 //this tells the server where to get the static files--the ones that don't change
