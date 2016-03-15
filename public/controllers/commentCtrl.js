cb7e7d57d7ce2e8ec13fb0f5303573721cea4f4b
schmitz1193 <schmitz1193@gmail.com>
2016-03-15 11:44:49 -0500

    see comments!

--- public/controllers/commentCtrl.js
+++ public/controllers/commentCtrl.js
@@ -4,12 +4,20 @@ app.controller("CommentCtrl", [
   '$scope',
   function($scope){
 
+    $scope.saveComment = function(){
+
+      console.log("Comment ", $scope.comment);
+
+    }
     //get all the comments from the users db that have the id of the shop we are  on.  I think I need to pass id with a factory
     //display all the comments
     //allow user to add/update/delete their own comment...need to pass that info in as well
 
-
-
+    $scope.comments = [
+        {author: 'Hillary', body: 'Women should rule!', upvotes: 0},
+        {author: 'Bill', body: 'What she said!', upvotes: 0},
+        {author: 'me', body: 'FYI not May I', upvotes: 0}
+    ];
 
 
 
