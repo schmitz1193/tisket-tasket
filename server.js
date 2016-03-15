b2ff2f7e0d21fdca8d4dbf4fe285b6a6e379b30e
schmitz1193 <schmitz1193@gmail.com>
2016-03-15 14:21:47 -0500

    re-direct to shop

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;  //set up environment port if none given default to 3000
-
-//test the server set route to index page
-app.get('/', function (req,res) {
-  res.send('Tisket Tasket keep me from the casket!')
-});
-
-
 //this tells the server where to get the static files--the ones that don't change
 app.use(express.static(__dirname + "/public"));
+
+//test the server set route to index page
+//
+// app.get('/', function (req,res) {
+//   res.send('help - anybody there?')
+// });
+
+// tells the server which template to use as the index & to listen for the get request and log the message in the command prompt
+app.post('/login', function(req,res){
+  console.log("I received the login request")
+  console.log("let's pretend the login is valid!");
+  res.redirect('/#/shop');
+});
+
 
 
 
